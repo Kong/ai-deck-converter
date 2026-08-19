cdd12ae9184a753e7a8c7802631e53f70e71ca86
Jay Shah <jay.shah@konghq.com>
2026-08-19 17:32:42 -0400

    remove description field

--- internal/aigw/ca_certificate.go
+++ internal/aigw/ca_certificate.go
@@ -3,10 +3,9 @@ package aigw
 // CACertificate is an AI Gateway CA certificate: a trusted CA used to verify
 // the validity of a client or server certificate.
 type CACertificate struct {
-	ID          string `yaml:"id,omitempty"`
-	Name        string `yaml:"name,omitempty"`
-	Cert        string `yaml:"cert,omitempty"`
-	CertDigest  string `yaml:"cert_digest,omitempty"`
-	Description string `yaml:"description,omitempty"`
-	Labels      Labels `yaml:"labels,omitempty"`
+	ID         string `yaml:"id,omitempty"`
+	Name       string `yaml:"name,omitempty"`
+	Cert       string `yaml:"cert,omitempty"`
+	CertDigest string `yaml:"cert_digest,omitempty"`
+	Labels     Labels `yaml:"labels,omitempty"`
 }
